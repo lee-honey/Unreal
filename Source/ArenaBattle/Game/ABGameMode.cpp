a0d4caf319414d5d42662fa884fc2726a206f39c
DREAM-HOME\kulee <dimplehh@naver.com>
2025-05-19 21:03:48 +0900

    first commit

---
+++ Source/ArenaBattle/Game/ABGameMode.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/ABGameMode.h"
+#include "ABGameMode.h"
+
+AABGameMode::AABGameMode()
+{
+	static ConstructorHelpers::FClassFinder<APawn> DefaultPawnClassRef(TEXT("/Script/Engine.Blueprint'/Game/ArenaBattle/Blueprint/BP_AB_CharacterPlayer.BP_AB_CharacterPlayer_C'"));
+	if (DefaultPawnClassRef.Class)
+	{
+		DefaultPawnClass = DefaultPawnClassRef.Class;
+	}
+
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerClassRef(TEXT("/Script/ArenaBattle.ABPlayerController"));
+	if (PlayerControllerClassRef.Class)
+	{
+		PlayerControllerClass = PlayerControllerClassRef.Class;
+	}
+}
