8a833184e440b2255eaaf8915fdc82e960f8e5fa
DREAM-HOME\kulee <dimplehh@naver.com>
2025-06-15 23:09:04 +0900

    13강 완강

--- Source/ArenaBattle/CharacterStat/ABCharacterStatComponent.cpp
+++ Source/ArenaBattle/CharacterStat/ABCharacterStatComponent.cpp
@@ -9,22 +9,22 @@ UABCharacterStatComponent::UABCharacterStatComponent()
 {
 	CurrentLevel = 1;
 	AttackRadius = 50.0f;
+
+	bWantsInitializeComponent = true;
 }
 
-
-// Called when the game starts
-void UABCharacterStatComponent::BeginPlay()
+void UABCharacterStatComponent::InitializeComponent()
 {
-	Super::BeginPlay();
+	Super::InitializeComponent();
 
-	SetLevelStat(CurrentLevel); // 초기값 설정
+	SetLevelStat(CurrentLevel);
 	SetHp(BaseStat.MaxHp);
 }
 
 void UABCharacterStatComponent::SetLevelStat(int32 InNewLevel)
 {
 	CurrentLevel = FMath::Clamp(InNewLevel, 1, UABGameSingleton::Get().CharacterMaxLevel);
-	BaseStat = UABGameSingleton::Get().GetCharacterStat(CurrentLevel);
+	SetBaseStat(UABGameSingleton::Get().GetCharacterStat(CurrentLevel));
 	check(BaseStat.MaxHp > 0.0f);
 }
 
